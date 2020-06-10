55c2162b887877dd01c5af86638dca80f20c69eb
PotatoEngineer <43867559+jinseo99@users.noreply.github.com>
2020-06-10 12:05:22 -0600

    Update WSJOB.sh

--- WSJOB.sh
+++ WSJOB.sh
@@ -5,9 +5,10 @@
 #SBATCH --time=22:00:00
 #SBATCH --output=output/job-%j.out
 
+source env/bin/activate
+
 echo "Start of Script"
 
-echo output
 echo "starting from index 4310"
 
 python summarizer.py
